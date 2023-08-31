ba051a707bfa765783837ac400060b7f8fb02449
Aditya Soni <adityasoni5859@gmail.com>
2023-08-31 22:46:43 +0530

    code refactor

--- main.py
+++ main.py
@@ -11,8 +11,5 @@ app = FastAPI()
 
 @app.post("/")
 async def create_item(request_body: RequestBody):
-    if request_body.text_str is None or request_body.text_str == '':
-        return {"status":"failed", "data": []}
-    else:
         result = np.random.uniform(low=0, high=10, size=(500,))
         return {"status":"success", "data": list(result)}

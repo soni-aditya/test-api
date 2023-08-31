0b3c2897ef241f95d597dbed139ddea9a0bcf8f1
Aditya Soni <adityasoni5859@gmail.com>
2023-08-31 22:43:09 +0530

    added readme

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# API considerations
+- Since only one api endpoint needed to be created, I have decided to create it in a single file rather than using an api template/architecture
+- For covering edgecases (like input string being null or empty), We are using RequestBody (which inherits BaseModel). This way, fastapi makes the type checking strict
+
+### The app can be launched using Gunicorn or Uvicorn
+eg. use command **uvicorn main:app --host 0.0.0.0 --port 80** to initiate a run
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 import numpy as np
 
 class RequestBody(BaseModel):
-    text_str: str | None = ''
+    text_str: str 
 
 
 app = FastAPI()
@@ -11,5 +11,8 @@ app = FastAPI()
 
 @app.post("/")
 async def create_item(request_body: RequestBody):
-    result = np.random.uniform(low=0, high=10, size=(500,))
-    return {"status":"success", "data": list(result)}
+    if request_body.text_str is None or request_body.text_str == '':
+        return {"status":"failed", "data": []}
+    else:
+        result = np.random.uniform(low=0, high=10, size=(500,))
+        return {"status":"success", "data": list(result)}
